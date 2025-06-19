def1951a3a6f001ed3ef8fdf6bc5c431256c6484
Maayank18 <maayank51518@gmail.com>
2025-06-19 21:44:17 +0530

    added leetcode

---
+++ Arrays/Leetcode_27.cpp
@@ -0,0 +1,54 @@
+// REMOVE DUPLICATE -> REMOVING ALL THE OCCURENCES
+
+
+// brute force / optimal
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int finding( vector<int> &nums, int val){
+    int n = nums.size();
+
+    int i = 0;
+    for(int j = 0; j<n; j++){
+        if(nums[j] != val){
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+
+    return nums.size();
+}
+
+int main(){
+    vector<int> nums = {0,1,2,2,3,0,4,2};
+    int val = 2;
+    int newSize = finding(nums,val);
+    cout<<" the new updated size is : "<<newSize<<endl;
+    return 0;
+}
+
+
+
+
+// best approach
+
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int finding( vector<int> &nums, int val){
+    nums.erase(remove(nums.begin(),nums.end(),val),nums.end());
+
+    return nums.size();
+}
+
+int main(){
+    vector<int> nums = {0,1,2,2,3,0,4,2};
+    int val = 2;
+    int newSize = finding(nums,val);
+    cout<<" the new updated size is : "<<newSize<<endl;
+    return 0;
+}
